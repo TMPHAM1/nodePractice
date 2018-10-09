7c28d25d7ccbecd99c4f5006158d4ce8defe4f74
Han-Mi Kim <tmpham1@uci.edu>
2018-10-09 15:37:43 -0700

    added error handling for non exisiting files

--- api/app.js
+++ api/app.js
@@ -4,27 +4,34 @@ var express = require("express");
 var app = express(); 
 var routes = require('./routes')
 
-   
+var jsonParser = require('body-parser').json
+var logger = require('morgan')
 
-    var jsonParser = require('body-parser').json
+app.use(logger("dev"));
 app.use(jsonParser())
-
 app.use("/questions", routes)
 
+
+//Sends a 404 if no other routes are found 
+app.use(function(req, res, next) {
+    var err = new Error("Nothing found");
+    err.status = 404; 
+    next(err);
+})
+
+// Error Handler 
+
+app.use(function(err,req,res,next) {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: err.message
+        }
+    })
+
+})
+
 var port = process.env.port || 3000; 
-
-app.use(function(req,res,next) {
-    console.log("first piece of middleware")
-    req.myMessage = "hello middleware 2"
-    console.log("The leaves on the trees are", req.query.color)
-    next();
-})
-
-app.use("/different/:id", function(req,res,next) {
-    console.log("second piece of middleware ID:", req.params.id);
-    console.log(req.myMessage);
-    next();
-})
  
 app.listen(port, function() {
     console.log("Express is listening on port", port)
--- api/routes.js
+++ api/routes.js
@@ -20,14 +20,59 @@ router.post("/", function(req, res) {
 
 });
 
-router.get("/:id", function(req, res) {
+router.get("/:qID", function(req, res) {
     // return all the questions 
     res.json({
-        response: "You sent me a get Request for ID" + req.params.id});
+        response: "You sent me a get Request for ID" + req.params.qID});
 
 
     
 });
 
+//POST questions/:qID/answers
+router.post("/:qID/answers", function(req, res) {
+    // return all the questions 
+    res.json({response: "You sent me a POST request to / answers",
+              questionId: req.params.qID,
+              body: req.body,
+
+    });
+});
+
+//PUT /questions/:qID/answers/:qID
+//Edit a specific answer
+router.put("/:qID/answers/:aID", function(req,res){
+
+    res.json({response: "You sent me a PUT request to / asnwers",
+               questionId: req.params.qID,
+               answerId: req.params.aID,
+            body: req.body})
+})
+router.delete("/:qID/answers/:aID", function(req,res){
+    res.json({response: "You sent me a DELETE request to / asnwers",
+               questionId: req.params.qID,
+               answerId: req.params.aID,
+    });
+});
+
+//POST /questions/:qIDanswers:aID/vote-up
+//POST /questions/:qIDanswers:aID/vote-down
+
+router.post("/:qID/answers/:aID/vote-:dir", function(req,res, next) {
+    if (req.params.dir.search(/^up|down$/) === -1) {
+        var err = new Error("not found")
+        err.status = 404; 
+        next(err);
+    }
+    else {
+        next();
+    }
+}  ,function(req,res){
+    res.json({response: "You sent me a DELETE request to /vote-" + req.params.dir,
+               questionId: req.params.qID,
+               answerId: req.params.aID,
+               vote: req.params.dir
+    });
+});
 
 module.exports = router;
