01919375cfb5f5f674a989d60ce15cbfe3b078d5
Han-Mi Kim <tmpham1@uci.edu>
2018-10-10 23:02:26 -0700

    setting up an object schema for mongoose

--- api/mongoose_sanbox.js
+++ api/mongoose_sanbox.js
@@ -7,10 +7,26 @@ mongoose.connect("mongoDB://localhost:27017/sandbox");
 var db = mongoose.connection;
 
 db.on("error", function() {
-    console.error("connection error")l;
+    console.error("connection error");
 }); 
 
-db.once("open");
+db.once("open", function() {
+    console.log("db connection successful");
+    //All Data base conenction function
+    var Schema = mongoose.Schema;
+    var AnimalSchema = new Schema({
+        type: String, 
+        size: String,
+        color: String, 
+        mass: Number, 
+        name: String
+    }); 
+    var Animal = mongoose.model("Animal", AnimalSchema); 
+
+    var elephant = new 
+
+    db.close(); 
+});
 
 
 
